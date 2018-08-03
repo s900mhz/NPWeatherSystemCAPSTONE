7ea08955581e285ce31bc4ef1314e15227dcc53e
Sean Spade <skspade@protonmail.com>
2018-08-02 21:06:07 -0400

    Got the javascript button to work!

---
+++ capstone/Capstone.Web/Scripts/ToggleSwitch.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $("#scale").change(function () {
+        let isF
+        if ($(this).prop('checked')) {
+            $(".f").toggle()
+            $(".c").toggle(false)
+        }
+        else {
+            $(".f").toggle(false)
+            $(".c").toggle()
+        }
+       
+        console.log(isF)
+    });
+
+});
